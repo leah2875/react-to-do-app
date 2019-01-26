ea6882adf1a5e0feec4a9969ecbea61d931e46d7
Leah Weile <leahweile@hotmail.com>
2019-01-26 12:30:25 -0800

    Checkpoint 8 React-Todo:events

--- src/App.js
+++ src/App.js
@@ -7,20 +7,48 @@ class App extends Component {
     super(props);
     this.state = {
       todos: [
-        { description: 'Walk the cat', isCompleted: true },
-        { description: 'Throw the dishes away', isCompleted: false },
-        { description: 'Buy new dishes', isCompleted: false }
-      ]
+        {  description: 'Walk the cat', isCompleted: true },
+        {  description: 'Throw the dishes away', isCompleted: false },
+        {  description: 'Buy new dishes', isCompleted: false }
+      ],
+        newTodoDescription: ''
     };
   }
+
+  handleChange(e) {
+     this.setState({ newTodoDescription: e.target.value })
+   }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.newTodoDescription) { return }
+    const newTodo = { description: this.state.newTodoDescription, isCompleted: false };
+    this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
+  }
+
+  toggleComplete(index) {
+     const todos = this.state.todos.slice();
+     const todo = todos[index];
+     todo.isCompleted = todo.isCompleted ? false : true;
+     this.setState({ todos:todos });
+   }
+
+   deleteTodo () {
+     const deleteItems = this.setState.filter({[ todos ]})
+   };
+
    render() {
      return (
        <div className="App">
         <ul>
         { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } onDelete={this.deleteTodo} />
           )}
         </ul>
+        <form onSubmit = { (e) => this.handleSubmit(e) }>
+       <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
+         <input type="submit" />
+       </form>
        </div>
      );
    }
